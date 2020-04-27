6fa145d35e83ba8f0b6050b9719c21c03139b220
Montival Junior <montival_junior@yahoo.com.br>
2020-04-27 11:13:44 -0300

    Envio de resolução desafio Criptografia/Descriptografia aceleradev Java utilizando Stream

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -20,33 +20,33 @@ public class CriptografiaCesariana implements Criptografia {
     }
 
     private String realizarDescriptografia(String texto) {
-        validartexto(texto);
+        validarTexto(texto);
         char[] arrayTexto = texto.toCharArray();
 
         for (char value : arrayTexto) listaTexto.add(Character.toString(value));
         return listaTexto.stream()
-                .map(c -> troca(0, c, -3))
+                .map(c -> modificarCaracter(0, c, -3))
                 .map(String::valueOf)
                 .collect(Collectors.joining()).toLowerCase();
     }
 
     private String realizarCriptografia(String texto) {
-        validartexto(texto);
+        validarTexto(texto);
         char[] arrayTexto = texto.toCharArray();
 
         for (char value : arrayTexto) listaTexto.add(Character.toString(value));
         return listaTexto.stream()
-                .map(c -> troca(0, c, 3))
+                .map(c -> modificarCaracter(0, c, 3))
                 .map(String::valueOf)
                 .collect(Collectors.joining()).toLowerCase();
     }
 
-    private char troca(int index, String lista, int codificador) {
+    private char modificarCaracter(int index, String lista, int codificador) {
         return (Character.isDigit(lista.charAt(index)) || !Character.isLetter(lista.charAt(index)))
                 ? lista.charAt(index) : (char) (lista.charAt(index) + codificador);
     }
 
-    private void validartexto(String texto) throws IllegalArgumentException {
+    private void validarTexto(String texto) throws IllegalArgumentException {
         if (texto.isEmpty()) {
             throw new IllegalArgumentException("Erro! Você deve informar um texto");
         }
