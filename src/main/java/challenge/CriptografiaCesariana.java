280df0e9a510bcfc3cbb292906a923edabec679b
Montival Junior <montival_junior@yahoo.com.br>
2020-04-27 10:56:47 -0300

    Envio de resolução desafio Criptografia/Descriptografia aceleradev Java utilizando Stream

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -1,54 +1,54 @@
 package challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CriptografiaCesariana implements Criptografia {
+    List<String> listaTexto = new ArrayList<>();
 
     @Override
     public String criptografar(String texto) {
 
-        return realizarCriptografia(texto, 3, 35);
+        return realizarCriptografia(texto);
     }
 
     @Override
     public String descriptografar(String texto) {
 
-        return realizarDescriptografia(texto, 3, 29);
+        return realizarDescriptografia(texto);
     }
 
-    private String realizarDescriptografia(String texto, int converteMinusculo, int converteMaiuculo) {
-        String textoDecifrado = "";
-        if (texto.isEmpty()) {
-            throw new IllegalArgumentException("Erro! Você deve informar um texto");
-        } else {
-            for (int i = 0; i < texto.length(); i++) {
-                if (Character.isDigit(texto.charAt(i)) || !Character.isLetter(texto.charAt(i))) {
-                    textoDecifrado += texto.charAt(i);
-                } else if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
-                    textoDecifrado += (char) (texto.charAt(i) - converteMinusculo);
-                } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
-                    textoDecifrado += (char) (texto.charAt(i) + converteMaiuculo);
-                }
-            }
-        }
+    private String realizarDescriptografia(String texto) {
+        validartexto(texto);
+        char[] arrayTexto = texto.toCharArray();
 
-        return textoDecifrado;
+        for (char value : arrayTexto) listaTexto.add(Character.toString(value));
+        return listaTexto.stream()
+                .map(c -> troca(0, c, -3))
+                .map(String::valueOf)
+                .collect(Collectors.joining()).toLowerCase();
     }
 
-    private String realizarCriptografia(String texto, int converteMinusculo, int converteMaiuculo) {
-        String textoCifrado = "";
+    private String realizarCriptografia(String texto) {
+        validartexto(texto);
+        char[] arrayTexto = texto.toCharArray();
+
+        for (char value : arrayTexto) listaTexto.add(Character.toString(value));
+        return listaTexto.stream()
+                .map(c -> troca(0, c, 3))
+                .map(String::valueOf)
+                .collect(Collectors.joining()).toLowerCase();
+    }
+
+    private char troca(int index, String lista, int codificador) {
+        return (Character.isDigit(lista.charAt(index)) || !Character.isLetter(lista.charAt(index)))
+                ? lista.charAt(index) : (char) (lista.charAt(index) + codificador);
+    }
+
+    private void validartexto(String texto) throws IllegalArgumentException {
         if (texto.isEmpty()) {
             throw new IllegalArgumentException("Erro! Você deve informar um texto");
-        } else {
-            for (int i = 0; i < texto.length(); i++) {
-                if (Character.isDigit(texto.charAt(i)) || !Character.isLetter(texto.charAt(i))) {
-                    textoCifrado += texto.charAt(i);
-                } else if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
-                    textoCifrado += (char) (texto.charAt(i) + converteMinusculo);
-                } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
-                    textoCifrado += (char) (texto.charAt(i) + converteMaiuculo);
-                }
-            }
         }
-
-        return textoCifrado;
     }
 }
